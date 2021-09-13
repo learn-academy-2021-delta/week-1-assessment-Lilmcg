e344af0f87532b242b89994dcc35f48bb915f76f
Lilmcg <lilmcg@gmail.com>
2021-09-12 19:23:44 -0700

    completed week 1 assessment

--- code-challenges.js
+++ code-challenges.js
@@ -7,42 +7,179 @@
 
 // --------------------1) Create a statement that determines if a temperature is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit.
 
+// If the temperature entered by the user is below boiling point then a message that the temperature value "is below boiling point" will be delivered.
+// If the temperature entered by the user is above boiling point then a message that the temperature value "is above boiling point" will be delivered.
+// If the temperature entered by the user is equal to boiling point then a message that the temperature value "is equal to boiling point" will be delivered.
+// Boiling point is 212 F
+
+// Declare the variable to be temperature equal to 212.
+var temp = 212
+
 // Create the code and test each of the variables provided. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp = 35
-// var temp = 350
-// var temp = 212
+// Declare temp the variable is equal to 35
+// Declare boilingPt the variable is equal to 212
+// If temp 35 is less than boilingPt 212
+   // Print "35 is below boiling point"
+// Else if temp 35 is greater than boilingPt 212
+   // Print "35 is above boiling point"
+// Else temp 35 is equal to boilingPt 212
+  // Print "35 is at boiling point"
 
+var temp = 35
+var boilingPt = 212
+if (temp < boilingPt) {
+  console.log (`${temp} is below boiling point`)
+} else if (temp > boilingPt) {
+  console.log (`${temp} is above boiling point`)
+} else if (temp = boilingPt) {
+  console.log (`${temp} is at boiling point`)
+}
+
+
+// Declare temp the variable is equal to 350
+// Take boilingPt variable previousy declared
+// If temp 350 is less than boilingPt 212
+   // print "350 is below boiling point"
+// Else if temp 35 is greater than boilingPt 212
+   // Print "350 is above boiling point"
+// Else temp 350 is equal to boilingPt 212
+  // Print "350 is at boiling point"
+
+var temp = 350
+var boilingPt = 212
+if (temp < boilingPt) {
+  console.log (`${temp} is below boiling point`)
+} else if (temp > boilingPt) {
+  console.log (`${temp} is above boiling point`)
+} else if (temp = boilingPt) {
+  console.log (`${temp} is at boiling point`)
+}
+
+// Declare temp the variable is equal to 212
+// Take boilingPt variable previousy declared
+// If temp 212 is less than boilingPt 212
+   // print "212 is below boiling point"
+// Else if temp 212 is greater than boilingPt 212
+   // print "212 is above boiling point"
+// Else temp 212 is equal to boilingPt 212
+  // Print "212 is at boiling point"
+
+var temp = 212
+var boilingPt = 212
+if (temp < boilingPt) {
+  console.log (`${temp} is below boiling point`)
+} else if (temp > boilingPt) {
+  console.log (`${temp} is above boiling point`)
+} else if (temp = boilingPt) {
+  console.log (`${temp} is at boiling point`)
+}
 
 
 // --------------------2) Create the code that will combine two arrays and return the length using the variables provided below. Expected output: 10
 
+// Declare first variable myNumbers1 is equal to an array of numbers
+// Declare second variable myNumbers2 is equal to an array numbers
+  // Print the length of the two combined arrays
+
 var myNumbers1 = [3, 7, 0, 36, -9]
 var myNumbers2 = [8, -7, 22, 9, 13]
-
+console.log((myNumbers1.concat(myNumbers2)).length)
 
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variables provided below. Expected output: "eilrahc", "atled"
 
-var myString1 = "charlie"
-var myString2 = "delta"
+// Declare myString1 as the variable is equal to "charlie"
+// Declare splitMyString1 as the variable is equal to split function of myString1
+// Declare reverseMyString1 as the variable is equal to reverse function of splitMyString1
+  // Print join function of reverseMyString1
 
+var myString1 = "charlie"
+var splitMyString1 = myString1.split("")
+var reverseMyString1 = splitMyString1.reverse()
+console.log(reverseMyString1.join(""))
+
+// Declare myString1 as the variable is equal to "delta"
+// Declare splitMyString1 as the variable is equal to split function of myString1
+// Declare reverseMyString1 as the variable is equal to reverse function of splitMyString1
+  // Print join function of reverseMyString1
+
+var myString2 = "delta"
+var splitMyString2 = myString2.split("")
+var reverseMyString2 = splitMyString2.reverse()
+console.log(reverseMyString2.join(""))
 
 
 // --------------------4) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 15, 12
 
+// If the first number entered by the user is greater than the second number entered by the user then a message with the value of, number 1 subtracted from number 2, will be delivered
+// If the second number entered by the user is greater than the first number entered by the user then a message with the value of, number 2 subtracted from number 1, will be delivered
+
+
+// Declare number1 as the variable is equal to the number value 42
+// Declare number2 as the variable is equal to number value 27
+  // If number1 is greater than number2
+    // Print value of number1 minus number2
+// Else print value of number2 minus number1
+
 var number1 = 42
 var number2 = 27
+if (number1 > number2) {
+  console.log(number1 - number2)
+} else {
+  console.log(number2 - number1)
+}
 
-// var number1 = 7
-// var number2 = 19
+// Take two variable declared previously
+// Declare number1 equal to number value 7
+// Declare number2 equal to number value 19
+  // If number1 is greater than number2
+    // Print value of number1 minus number2
+// Else print value of number2 minus number1
 
+var number1 = 7
+var number2 = 19
+if (number1 > number2) {
+  console.log(number1 - number2)
+} else {
+  console.log(number2 - number1)
+}
 
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to also accept non-number edge cases. Inform your user if the variable is not a number using the two sets of test variables provided below. Expected output: "Your input is not a number", 3
 
+// Declare variable numberRefactor1 equal to number value 42
+// Declare variable numberRefactor2 equal to the string hello
+  // If numberRefactor1 is not a number or numberRefactor2 is not a number
+    // Print "your input is not a number"
+// Else if numberRefactor1 is greater than numberRefactor2
+   // Print value of numberRefactor1 minus numberRefactor2
+// Else print value of numberRefactor2 minus numberRefactor1
+
 var numberRefactor1 = 42
 var numberRefactor2 = "hello"
+if (isNaN (numberRefactor1) || isNaN (numberRefactor2)) {
+  console.log ("Your input is not a number")
+} else if (numberRefactor1 > numberRefactor2) {
+  console.log(numberRefactor1 - numberRefactor2)
+} else {
+  console.log(numberRefactor2 - numberRefactor1)
+}
 
-// var numberRefactor1 = 27
-// var numberRefactor2 = 24
+// Declare variable numberRefactor1 equal to number value 27
+// Declare variable numberRefactor2 equal to number value 24
+  // If numberRefactor1 is not a number or numberRefactor2 is not a number
+    // Print "your input is not a number"
+// Else if numberRefactor1 is greater than numberRefactor2
+   // Print value of numberRefactor1 minus numberRefactor2
+// Else print value of numberRefactor2 minus numberRefactor1
+
+var numberRefactor1 = 27
+var numberRefactor2 = 24
+if (isNaN (numberRefactor1) || isNaN (numberRefactor2)) {
+  console.log ("Your input is not a number")
+} else if (numberRefactor1 > number2) {
+  console.log(numberRefactor1 - numberRefactor2)
+} else {
+  console.log(numberRefactor2 - numberRefactor1)
+}
--- coding-concepts.js
+++ coding-concepts.js
@@ -8,44 +8,57 @@
 // --------------------1) What will this log?
 
 var cohort = "Delta 2021"
-// console.log(cohort.length)
+console.log(cohort.length)
 
 // a) Your answer:
-// b) Verify and explain:
+// 10
 
+// b) Verify and explain:
+// Initial answer was correct // Counted every character and space in the var cohort variable string to end up with a length of 10 to print on the console // Started counting length at 1
 
 // --------------------2) What will this log?
 
 var greeting = "Hello World!"
-// console.log(greeting[3])
+console.log(greeting[3])
 
 // a) Your answer:
+// l
+
 // b) Verify and explain:
+// Initial answer was correct // greeting[3] in console.log will print the character at index 3 of the string // started index count at 0
 
 
 // --------------------3) What will this log?
 
 var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
-// console.log(languages[index])
+console.log(languages[index])
 
 // a) Your answer:
-// b) Verify and explain:
+// Ruby
 
+// b) Verify and explain:
+// Initial answer was correct // console.log will print the value at index 1 in the var languages array // started index count at 0
 
 // --------------------4) What will this log?
 
 var weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+console.log(weekendDays.toUpperCase())
 
 // a) Your answer:
-// b) Verify and explain:
+// SATURDAY
+// SUNDAY
 
+// b) Verify and explain:
+// Initial answer was wrong // terminal returned an error message // toUpperCase function ran into a problem attempting to uppercase the array // javascript requires more info before it can perform this function in an array
 
 // --------------------5) What will this log?
 
 var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
 // a) Your answer:
+// number
+
 // b) Verify and explain:
+// Initial answer was correct // the typeof data printed by the .length function will be a type of number
